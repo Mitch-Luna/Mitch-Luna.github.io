99c97aa0f8d773edd1be78f4757d3b688794ba1f
Mitch-Luna <lunitamitch02@gmail.com>
2024-10-16 11:08:31 -0600

    galeria de fotos2

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function actualizarContador() {
     const segundos = Math.floor((diferencia % (1000 * 60)) / 1000);
 
     // Actualiza el contenido en el HTML
-    contadorElement.textContent = `${dias} Días, ${horas} Horas, ${minutos} Minutos y ${segundos} Segundos transcurridos desde el 18 de septeimbre 2024`;
+    contadorElement.textContent = `${dias} Días, ${horas} Horas, ${minutos} Minutos y ${segundos} Segundos transcurridos desde el 18 de septiembre 2024`;
 }
 
 // Actualiza el contador cada segundo
